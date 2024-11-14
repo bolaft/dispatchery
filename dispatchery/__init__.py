a0d7871756384645a1da718f372a9931c7191856
Soufian SALIM <soufian.salim@bee4win.com>
2024-11-15 00:41:03 +0100

    v0.2.0, previous commit name was a typo

--- dispatchery/__init__.py
+++ dispatchery/__init__.py
@@ -4,6 +4,6 @@
 Dispatchery package.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 from .main import dispatchery
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dispatchery",
-    version="0.1.2",
+    version="0.2.0",
     description="A Python package for advanced function dispatching based on complex, nested, and parameterized types. Inspired by functools.singledispatch.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
